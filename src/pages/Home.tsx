0d9f6c939630b36886863f535d122e7e9f82e21d
jorgenrussnes <jrussnes@gmail.com>
2021-10-06 18:40:32 +0200

    Updated redirect url

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import Header from '../components/header'
 const { REACT_APP_CLIENT_ID } = process.env;
 const redirectUrl = process.env.NODE_ENV === "development"
     ? "http://localhost:3000/redirect"
-    : "https://appfrontendsa.z6.web.core.windows.net/redirect";
+    : "https://strava-app1998.web.app/redirect";
 
 const handleLogin = () => {
     window.location.href = `http://www.strava.com/oauth/authorize?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${redirectUrl}/exchange_token&approval_prompt=force&scope=read_all,activity:read_all`;
